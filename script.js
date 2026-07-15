037403b3534beff72eafa615c31dd4307d7d3f8c
Vishal <120316470+VishalRpR@users.noreply.github.com>
2026-07-15 09:46:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-function mincost(arr)
-{ 
-	arr.sort()
-	let cost=0;
-  let newarr=arr.map((item,i)=>{
-	  cost+=item;
-	  return cost;
-})
+function mincost(arr) {
+    arr.sort((a, b) => a - b);
 
+    let cost = 0;
 
-	let sum=0;
-	newarr.shift();
-	newarr.forEach((item)=>{
-        sum+=item;
-	})
+    while (arr.length > 1) {
+        // Take the two smallest ropes
+        let first = arr.shift();
+        let second = arr.shift();
 
-	return sum;
-  
+        let sum = first + second;
+        cost += sum;
+
+        // Insert the new rope back
+        arr.push(sum);
+        arr.sort((a, b) => a - b);
+    }
+
+    return cost;
 }
 
 
